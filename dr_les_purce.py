abfb209eda73a539138c5ec6e2fb5e3c2da6c3b7
Jay R Bolton <jayrbolton@gmail.com>
2013-02-13 15:07:57 -0800

    changed some verbiage. Added a bunch of generic responses

--- dr_les_purce.py
+++ dr_les_purce.py
@@ -11,7 +11,7 @@ def generate_evergreen_class(matches, messenger):
 	# lists of evergreen class words.
 	# these must be gerunds of transitive verbs. for example 'looking' doesn't work ('looking social diversity').
 	gerunds = ["Discovering", "Imagining", "Drawing", "Defending", "Writing", "Rewriting", "Interrogating", "Examining", "Reinterpreting", "Engaging", "Exploring", "Interpreting", "Improvising"]
-	adjectives = ["Real", "Literary", "Imagined", "Cultural", "Evolutionary", "Social", "Forbidden", "Sustainable", "Emergent", "Dynamic", "Interdisciplinary", "Growing", "Sustainable", "Gothic", "An Introduction To", "Non-postmodern","Modern"]
+	adjectives = ["Real", "Literary", "Imagined", "Cultural", "Evolutionary", "Social", "Forbidden", "Sustainable", "Emergent", "Dynamic", "Interdisciplinary", "Growth-based", "Sustainable", "Gothic", "An Introduction To", "Non-postmodern","Modern"]
 	nouns = ["Alternatives", "Foundations", "Representations", "Narratives", "Crafts", "Biodiversity", "Knowledge", "Abjection", "Art", "Time", "Argentina", "Plants", "People", "Ecoliteracy", "Change", "Mausoleums", "Systemizations", "Equality Speech", "Male Privelege", "Gender-Power", "Freewriting", "Media Studies", "Studies", "Afro-Brazilian Dance", "Approaches to Healing", "Ballet", "Art/Work", "Foundations of Change", "Form", "Function", "Dance Creations", "Power Dynamics", "Heteronormativity", "Inverted Sexuality", "Sexuality", "Polyamory", "Freestyle Deforestation"]
 
 	# random pop
@@ -35,11 +35,9 @@ def generate_evergreen_class(matches, messenger):
 	]) # you could abstract out the prepositions [and, of] if you wanted
 	return random.choice(intros) + formats() # end generate_evergreen_class
 
-def random_response(matches, messenger):
-	return random.choice([generate_evergreen_class(matches,messenger), insulted(matches,messenger), generic(matches,messenger)])
-
+# Response totally not driven by context. Placeholders until he gets smarter.
 def generic(matches, messenger):
-	return random.choice(["Let's all hold hands in red square.","You are the future.", "I have my own secret cave in the evergreen woods.", "I am immortal."])
+	return random.choice(["Let's all hold hands in red square.","You are the future.", "I have my own secret cave in the evergreen woods.", "I am immortal.","We can work through this together.","I wrote a song for you on my guitar.","This sounds like a good topic for a rap song.", "Beedoop badop bop", "As Martin Luther King Jr said 'everyone loves a good pie'", "I sleep in a lean-to in the woods. Bee bop badoobop.", "Good point. Let's have a seminar about it"])
 
 def insulted(matches, messenger):
 	return random.choice(["Talk to my mustache.","Let me console you with my guitar.","Check your pie plate. Did you grease it?","Talk to my secretary.","Art Constantino will handle this.", "Better grease yo pie plate", "You need more interdisciplinary education"])
@@ -48,10 +46,10 @@ def insulted(matches, messenger):
 # def is_420():
 # 	return time.strftime("%H%M") == "1620"
 
-bot = Bot('irc.freenode.net',6667,'DrLesPurce_test','les','Dr Les Purce (bot)')
-bot.join('#jayparty')
+bot = Bot('irc.freenode.net',6667,'DrLesPurce','les','Dr Les Purce (1.1)')
+bot.join('#tesc')
 bot.bind('DrLesPurce:.*fuck(ing)?|shit|dammit|damn|stupid|dumb|lame.*', insulted)
 bot.bind('DrLesPurce: .*\?', generate_evergreen_class) # When asked a question, suggest to join a program
-bot.bind('DrLesPurce: .*', random_response) # fallback: say something random
+bot.bind('DrLesPurce: .*', generic) # fallback: say something random
 
 bot.live()
